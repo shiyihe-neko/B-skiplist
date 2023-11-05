c73ff50a0e72a117aac89e1646666d321452e481
Robert Li <1067907540@qq.com>
2023-11-04 20:38:21 -0600

    update the base structure implemented insert(not tested)

--- BSkipList.cpp
+++ BSkipList.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
+#include <ctime>
+#include <vector>
 
 class Node {
 public:
     int value;
     Node* next; // Pointer to next node within the same block
-    Node* above; // Pointer to the node above in the upper block
+    Node* down; // Pointer to the corresponding node in the lower level
 
-    Node(int val) : value(val), next(nullptr), above(nullptr) {}
+    Node(int val) : value(val), next(nullptr), down(nullptr) {}
 };
 
 class Block {
 public:
-    std::vector<Node*> nodes; // Container for nodesA within the block
-    Block* next_block; // Pointer to the next block at the same level
-    Block* above_block; // Pointer to the block above at the upper level
+    Node* head; // Pointer to the first node of the block
+    Block* next; // Pointer to the next block at the same level
     size_t size; // Current size of the block
 
-    Block() : next_block(nullptr), above_block(nullptr), size(3) {}
+    Block() : head(nullptr), next(nullptr), size(3) {}
 
-    bool is_full() const {
-        return nodes.size() >= size;
+    bool is_full() {
+        Node* temp = head;
+        size_t count = 0;
+        while (temp) {
+            count++;
+            if (count >= size) {
+                return true;
+            }
+            temp = temp->next;
+        }
+        return false;
     }
 
     void double_size() {
@@ -29,13 +38,15 @@ public:
     }
 };
 
+
 class BSkipList {
 private:
-    Block* head;
+    std::vector<Block*> levels; // Vector of head blocks, one for each level
 
 public:
     BSkipList() {
-        head = new Block(); // Starting block at the bottom level
+        levels.push_back(new Block()); // Starting block at the bottom level
+        srand(static_cast<unsigned>(time(nullptr))); // Seed the random number generator
     }
 
     ~BSkipList() {
@@ -44,9 +55,78 @@ public:
     }
 
     void insert(int value) {
-        // ... (implementation of insert logic)
+        // Find the position to insert the new value in the bottom level
+        Block* current = levels[0];
+        Node* prev = nullptr;
+        while (current && (prev == nullptr || prev->value < value)) {
+            prev = find_position_in_block(current, value);
+            if (prev && prev->value == value) {
+                // Duplicate found; handle as required
+                return;
+            }
+            if (prev && prev->next && prev->next->value < value) {
+                current = current->next;
+            }
+        }
+
+        // Create the new node
+        Node* new_node = new Node(value);
+
+        // Insert the node in the correct position
+        if (prev) {
+            new_node->next = prev->next;
+            prev->next = new_node;
+        }
+        else {
+            new_node->next = current->head;
+            current->head = new_node;
+        }
+
+        // Adjust the block size if needed
+        if (current->is_full()) {
+            current->double_size();
+        }
+
+        // Coin flip to decide if we should promote
+        if (rand() % 2 == 0) {
+            promote(new_node);
+        }
     }
 
+    // Utility to find the position to insert within the block
+    Node* find_position_in_block(Block* block, int value) {
+        Node* current = block->head;
+        Node* prev = nullptr;
+        while (current && current->value < value) {
+            prev = current;
+            current = current->next;
+        }
+        return prev;
+    }
+
+    // Promote the node to the new block in the upper level
+    void promote(Node* node_to_promote) {
+        int level = 1; // Start promoting to the first level
+        Node* current_node = node_to_promote;
+
+        while (rand() % 2 == 0) { // Continue flipping the coin for promotion
+            if (level >= levels.size()) {
+                levels.push_back(new Block()); // Create a new level
+            }
+
+            // Create a new node for the upper level
+            Node* new_node = new Node(node_to_promote->value);
+            new_node->down = current_node; // Set down pointer to the current node
+
+            // Insert the new node into the block at the beginning
+            Block* current_level = levels[level];
+            new_node->next = current_level->head;
+            current_level->head = new_node;
+
+            current_node = new_node; // Move up for the next iteration
+            level++; // Go to the next level
+        }
+    }
     bool delete_value(int value) {
         // ... (implementation of delete logic)
     }
