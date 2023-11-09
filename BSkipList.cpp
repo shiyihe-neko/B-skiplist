12d657ca00283198837fbe2ac53d0a413b08fb18
Di Wu <u1163241@utah.edu>
2023-11-08 19:59:12 -0700

    Fix bug in one level

--- BSkipList.cpp
+++ BSkipList.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <random>
 #include<time.h> 
 
@@ -46,6 +47,7 @@ class BSkipList {
 private:
     std::vector<Block*> levels; // Vector of head blocks from each level
 public:
+    int r = 1;
     BSkipList() {
         Block* block = new Block(INT_MIN, nullptr); // negative infinity block
         levels.push_back(block);
@@ -58,14 +60,14 @@ public:
 
     void insert(int value) {
         srand(time(NULL)); // initialize random seed
-        int level = levels.size()-1;
-        Block* current = levels[level];  // first block in higest level
-        Block* block = levels[level];  // keep track the place for value
+        Block* current = levels[levels.size()-1];  // starting from first block in higest level
+        std::stack<Block*> blocks;      // place for value at each level
+        Block* block = levels[levels.size()-1];  // keep track the place for value
         while(current){
+            // find a value greater than insert value
             for(unsigned int i = 0; i < current->vector.size(); i++) {
                 if(value >= current->vector[i]->value){ // find the place
                     block = current;
-                    level --;
                     break;
                 }
             }
@@ -74,17 +76,18 @@ public:
         }
         // reach the level 0, block is place for value
         for(unsigned int i = 0; i < block->vector.size(); i++) {
-            if(block->vector[i]->value >= value){ // in the middle of the vector
-                if(rand() % 2 == 0) // tail
+            if(block->vector[i]->value > value){ // in the middle of the vector
+                if(r % 2 == 0){ // tail
+                    r++;
                     block->vector.insert(block->vector.begin() + i, new Node(value,nullptr));
+                }
                 else{ // head
+                    r++;
                     std::vector<Node*> right;
                     // split and shrink block
                     for(unsigned int j = i; j < block->vector.size(); j++)
                         right.push_back(block->vector[j]);
-                    std::cout << block->vector[0]->value <<std::endl;
-                    block->vector.resize(i+1);
-                    std::cout << block->vector[0]->value <<std::endl;
+                    block->vector.resize(i);
                     Block* rightBlock = new Block(-1,nullptr);
                     rightBlock->vector = right;
                     block->next = new Block(value,rightBlock);
@@ -93,9 +96,12 @@ public:
             }
         }
         // at the end of the vector
-        if(rand() % 2 == 0) // tail
+        if(r % 2 == 0){ // tail
+            r++;
             block->vector.push_back(new Node(value,nullptr));
+        }
         else{ //head
+            r++;
             Block* newBlock = new Block(value,block->next);
             block->next = newBlock;
         }
@@ -115,6 +121,9 @@ int main() {
     list.insert(5);
     list.insert(2);
     list.insert(3);
+    list.insert(4);
+    list.insert(4);
+    list.insert(5);
     list.insert(10);
     list.insert(-1);
     list.print();
